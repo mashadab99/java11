6d644188c9c7b3691a4ece5d8ec5d5c3cfe286c3
mashadab99 <mashadab99@gmail.com>
2021-07-03 12:38:04 +0530

    Lambda Expression Basic

---
+++ src/lambdaExpressions/example2/LambdaDemo2.java
@@ -0,0 +1,21 @@
+package lambdaExpressions.example2;
+
+public class LambdaDemo2 {
+    public static void main(String[] args){
+        NumericTest numericTest;
+
+        //A lambda expression to test if a number is even
+        numericTest = (number) -> (number%2) == 0;
+
+        System.out.println("10 is even "+numericTest.test(10));
+
+        System.out.println("9 is even "+numericTest.test(9));
+
+        //A lambda expression to test if a number is non negative
+        numericTest = (number) -> number >=0;
+
+        System.out.println("10 is non negative "+numericTest.test(10));
+
+        System.out.println("-1 is non negative "+numericTest.test(-1));
+    }
+}
---
+++ src/lambdaExpressions/example2/NumericTest.java
@@ -0,0 +1,6 @@
+package lambdaExpressions.example2;
+
+//Demonstrate a lambda expression that takes a parameters
+public interface NumericTest {
+    boolean test(int n);
+}
